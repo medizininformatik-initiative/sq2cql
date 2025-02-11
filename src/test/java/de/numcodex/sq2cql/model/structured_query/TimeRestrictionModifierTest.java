61f31eb26fb2377b5a53b97ef88516fcbcd456ff
Alexander Kiel <alexanderkiel@gmx.net>
2025-02-11 20:58:53 +0100

    Add type safe path resolving for time restrictions
    Closes: #164

--- src/test/java/de/numcodex/sq2cql/model/structured_query/TimeRestrictionModifierTest.java
+++ src/test/java/de/numcodex/sq2cql/model/structured_query/TimeRestrictionModifierTest.java
@@ -1,5 +1,6 @@
 package de.numcodex.sq2cql.model.structured_query;
 
+import de.numcodex.sq2cql.model.Mapping;
 import de.numcodex.sq2cql.model.MappingContext;
 import de.numcodex.sq2cql.model.cql.StandardIdentifierExpression;
 import org.junit.jupiter.api.Nested;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import java.time.LocalDate;
 
 import static de.numcodex.sq2cql.Assertions.assertThat;
+import static de.numcodex.sq2cql.model.Mapping.TimeRestriction.Type.*;
 
 class TimeRestrictionModifierTest {
 
@@ -17,7 +19,7 @@ class TimeRestrictionModifierTest {
 
         @Test
         void effective() {
-            var timeRestriction = TimeRestrictionModifier.of("effective", LocalDate.of(2021, 1, 1), LocalDate.of(2021, 1, 2));
+            var timeRestriction = TimeRestrictionModifier.of(Mapping.TimeRestriction.of("effective", DATE_TIME, PERIOD), LocalDate.of(2021, 1, 1), LocalDate.of(2021, 1, 2));
             var identifier = StandardIdentifierExpression.of("O");
 
             var expression = timeRestriction.expression(MappingContext.of(), identifier);
@@ -38,7 +40,7 @@ class TimeRestrictionModifierTest {
         @Test
         @DisplayName("recordedDate has no Period type")
         void recordedDate() {
-            var timeRestriction = TimeRestrictionModifier.of("recordedDate", LocalDate.of(2021, 1, 1), LocalDate.of(2021, 1, 2));
+            var timeRestriction = TimeRestrictionModifier.of(Mapping.TimeRestriction.of("recordedDate", DATE_TIME), LocalDate.of(2021, 1, 1), LocalDate.of(2021, 1, 2));
             var identifier = StandardIdentifierExpression.of("C");
 
             var expression = timeRestriction.expression(MappingContext.of(), identifier);
@@ -52,6 +54,26 @@ class TimeRestrictionModifierTest {
                     
                     define Criterion:
                       ToDate(C.recordedDate as dateTime) in Interval[@2021-01-01, @2021-01-02]
+                    """);
+        }
+
+        @Test
+        @DisplayName("potential date type test")
+        void dateTest() {
+            var timeRestriction = TimeRestrictionModifier.of(Mapping.TimeRestriction.of("date", DATE), LocalDate.of(2021, 1, 1), LocalDate.of(2021, 1, 2));
+            var identifier = StandardIdentifierExpression.of("X");
+
+            var expression = timeRestriction.expression(MappingContext.of(), identifier);
+
+            assertThat(expression.moveToPatientContext("Criterion")).printsTo("""
+                    library Retrieve version '1.0.0'
+                    using FHIR version '4.0.0'
+                    include FHIRHelpers version '4.0.0'
+                    
+                    context Patient
+                    
+                    define Criterion:
+                      ToDate(X.date as date) in Interval[@2021-01-01, @2021-01-02]
                     """);
         }
     }
